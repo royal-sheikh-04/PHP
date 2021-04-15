07d97bcbefda65ef87260a833859b127de74ec9b
Arman <58747083+royal-sheikh-04@users.noreply.github.com>
2021-04-16 01:27:50 +0530

    Add files via upload

---
+++ goto statement.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "Hello Arman Sheikh<br>";
+echo "Hello Arman Sheikh<br>";
+goto abc;				#goto statement ko aap use krte ho to goto ke baad vale statement print nahi hogi
+echo "Hello Arman Sheikh<br>";
+echo "Hello Arman Sheikh<br>";
+echo "Hello Arman Sheikh<br>";
+echo "Hello Arman Sheikh<br>";
+
+abc:
+echo "Hey this is  label ABC.";
+?>
